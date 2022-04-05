c563ddc8a075f5813bc1bc8ce225e651ac6165c7
Ryan Bester <34910027+ryanbester@users.noreply.github.com>
2022-04-05 21:17:51 +0100

    Initial commit

---
+++ DarkModeSample/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        DarkMode.WndProc(Me, m, DarkMode.DarkMode.Theme.SYSTEM)
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(Me, "Hello")
+    End Sub
+End Class
