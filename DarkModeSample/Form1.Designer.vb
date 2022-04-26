ce2a1ad1a71703be97b5cee2e2808f217908dedf
Ryan Bester <34910027+ryanbester@users.noreply.github.com>
2022-04-26 16:30:16 +0100

    Update DarkMode NuGet dependency

--- DarkModeSample/Form1.Designer.vb
+++ DarkModeSample/Form1.Designer.vb
@@ -27,6 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.Button1.Location = New System.Drawing.Point(12, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
